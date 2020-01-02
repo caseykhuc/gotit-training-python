444801a40a6e22285dec73683f30cefdb6bea58d
Casey Khuc <khuctrang1812@gmail.com>
2020-01-02 11:44:48 +0700

    feat: Add algo to update json data

--- practice-4/storefront_config.py
+++ practice-4/storefront_config.py
@@ -1,8 +1,26 @@
-class StorefrontConfig():
+def update(current, changes):
+    # update dictionary by a dictionary
+
+    if type(changes) == list:
+        for i in range(len(changes)):
+            update(current[i], changes[i])
+        return
+    if type(current) == dict:
+        for k, v in changes.items():
+            if type(v) != dict and type(v) != list:
+                # assign to new value when v is not of collection types
+                current[k] = changes[k]
+            else:
+                update(current[k], changes[k])
+        return
+
+
+""" Class model entities for data """
+class StorefrontConfig:
     # __init__(data: object)
     def __init__(self, data):
         self.data = data
 
     # update(modify_data: object)
     def update(self, modify_data):
-        self.data.update(modify_data)
+        update(self.data, modify_data)
